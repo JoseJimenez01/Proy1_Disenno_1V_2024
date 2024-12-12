eb1bb98e23ec4ca84e3834be25973d9f868f7ce4
JoseJimenez01 <jg3633110@gmail.com>
2024-12-11 19:25:56 -0600

    Plantilla
    Misma platilla con otro nombre

--- SolutionProy1WebYMobil/Proy1WebYMobil/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proy1WebYMobil.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
